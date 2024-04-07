d11b83493213bd07318f4351d37fc2bfaf6900c0
memeToasty <memeToasty@gmail.com>
2024-04-08 00:34:33 +0200

    modify server

--- ST.py
+++ ST.py
@@ -5,6 +5,8 @@
 import pyvjoy
 import socket
 import threading
+import time
+import re
 
 buttonsPerClient = 3
 axisPerClient = 2
@@ -52,27 +54,51 @@ class ClientThread(threading.Thread):
         self.socket.send(b"\nWelcome to the server\n\n")
 
         data = "not empty"
+        leftover = ""
+        pattern = r'^<\d+;\s*\d+;\s*\d+>'
 
         while len(data):
-            data = self.socket.recv(2048)
-            split = data.split(b";")
+            print("Waiting for data")
+            data = self.socket.recv(25).decode("utf-8")
+            print("Client sent:" + str(data))
+            data = leftover + data
+            print("Concatenated:" + str(data))
+            matches = re.findall(pattern, str(data))
+            packet = ""
+            if len(matches) > 0:
+                packet = matches[0]
+                print("Packet:" + str(packet))
+
+            leftover = data.replace(packet, "")
+            print("Leftover: " + str(leftover))
+            if packet == "":
+                print("No full packet received")
+                continue
+            curr_pack = packet.rstrip(">").lstrip("<")
+            print("Current packet:" + curr_pack)
+            try:
+                split = curr_pack.split(";")
+            except:
+                print("Split Error")
+            print("Processed:" + str(split))
             buttons = int(split[0])
             if buttons >= pow(2, buttonsPerClient):
                 continue
-            print("Client sent : " + str(data))
             j.data.lButtons = j.data.lButtons & ~((pow(2, buttonsPerClient) - 1) << self.offsetB)  # reset
             j.data.lButtons = j.data.lButtons | (buttons << self.offsetB)  # set
 
             for i in range(1, axisPerClient+1):
                 setAxis(i+self.offsetA, int(split[i]))
             j.update()
-            self.socket.send(b"Server received : " + data)
+            time.sleep(0.1)
+            self.socket.send(b"Ack")
+            print("Sent Ack")
 
         print("Client disconnected...")
         threads.remove(self)
 
 
-host = "127.0.0.1"
+host = "0.0.0.0"
 port = 9999
 
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
